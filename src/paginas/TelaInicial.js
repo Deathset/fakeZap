3d2142a359c3042dab47ba33803020ad9ea6c7ed
Davidsampler <davidsampler.engenharia@gmail.com>
2020-01-22 11:17:49 -0300

    telainicial

---
+++ src/paginas/TelaInicial.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react"
+import { View } from "react-native"
+import { StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native'
+
+export default class TelaInicial extends Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+
+                <TextInput style={styles.input}  placeholder='Nome Usuário' />
+                <TextInput style={styles.input} placeholder='Telefone' />
+
+                <TouchableOpacity style={styles.btn} onPress={this._onPressButton} >
+                <Text style={{fontSize:19,color:'white'}}>Login</Text>    
+                </TouchableOpacity>           
+                 
+                
+            </View>
+        )
+    }
+
+
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, backgroundColor: '#3F2F8F', justifyContent: "center", alignContent: "center"
+    },
+    input: {
+        backgroundColor: '#ffff', borderRadius: 5, marginTop: 30, marginLeft: 30, marginRight: 30
+    },
+    btn: {
+
+        alignItems: 'center',
+        backgroundColor: '#2C2068',
+        padding: 15,margin:30,marginTop:40,borderRadius:60,shadowColor:"#fff"        
+        
+    }
+})
+
---
+++ src/paginas/TelaMensagem.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import { View, ScrollView, TextInput, Button, Text, Dimensions, SafeAreaView, FlatList } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import socketIoClient from 'socket.io-client'
+
+//import service from './service'
+export default class TelaMensagem extends Component {
+
+  constructor(props){
+    super(props)
+    this.state ={
+      msg:'',
+      //socket: socketIoClient(service.server)
+    }
+
+
+  }
+  componentDidMount(){
+
+     /*
+     this.state.socket.emit('lastMessagers')
+
+     this.state.socket.on('newMenssages', msg => {
+       console.log(msg)
+     })*/
+
+  } 
+
+  getMessager(){
+    
+  }
+
+  async sendMessager(){
+    /*let resp = await service.post('/send',this.state.msg);
+    this.state.socket.emit('send', this.state.msg)
+    this.setState({msg:''})*/
+  }
+
+  render() {
+    return (
+        <View style={{ flex:1 }}>
+          <View style={{flex:1, backgroundColor:'green'}}>
+            <FlatList/>
+          </View>
+            <View style={{flex:0.13, flexDirection:'row', alignItems:'center'}}>
+                <TextInput
+                onChangeText={ msg => this.setState({msg})}
+                value={this.state.msg}
+                style={{flex:1, borderColor:'#000' ,borderWidth:0.2, borderRadius:20, marginHorizontal:5}}
+                />
+                <Button
+                onPress={() => { this.sendMessager()}}
+                title={'send'}
+                />
+        </View>
+    </View>
+    )
+  }
+
+}
